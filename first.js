52d702740a4fe8d13cac7fbf1cfd6aeb4f4d489b
KevalPatel8899 <200393482@student.georgianc.on.ca>
2018-12-21 13:48:35 -0500

    First Commit

---
+++ first.js
@@ -0,0 +1,4 @@
+var name=prompt("What is your first name");
+var lastName=prompt("what is your last name");
+var age=prompt("Enter your age");alert("naice to meet you "+name);
+console.log(name+" "+lastName+" your age is "+age);
