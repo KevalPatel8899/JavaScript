7af480933086db77fd5b43c66aa8c536ffe9a8e1
KevalPatel8899 <200393482@student.georgianc.on.ca>
2018-12-22 12:35:29 -0500

    Comments

--- first.js
+++ first.js
@@ -1,4 +1,8 @@
+// defien the variable and create a prompt to enter the name last name and the age
 var name=prompt("What is your first name");
 var lastName=prompt("what is your last name");
 var age=prompt("Enter your age");alert("naice to meet you "+name);
+
+
+// create the console log to show the the values which have entered
 console.log(name+" "+lastName+" your age is "+age);
