5bdcc284d64383f8b93d8369a08d5d6b0702ec3f
KevalPatel8899 <200393482@student.georgianc.on.ca>
2019-01-07 11:45:18 -0500

    Annoymet

---
+++ annoy.js
@@ -0,0 +1,12 @@
+//create a variable for the Propmt
+var ans = prompt("Are you ready!!?");
+
+
+//create a while loop to make an annoying loop until you don't give the answer we need
+
+while(ans !=="yes" && ans !=="yeah"){
+	var ans = prompt("Are you ready!!?");
+}
+//Throw an alert for the we made it
+	alert("Yes, We made it");
+
