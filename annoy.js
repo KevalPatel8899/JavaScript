4ae819680532429e0344f0d7a9cfa8b397d3fb7a
KevalPatel8899 <200393482@student.georgianc.on.ca>
2019-01-07 11:51:30 -0500

    IndexOf

--- annoy.js
+++ annoy.js
@@ -1,12 +1,15 @@
+
 //create a variable for the Propmt
-var ans = prompt("Are you ready!!?");
+
+ var ans = prompt("Are you ready!!?");
 
 
-//create a while loop to make an annoying loop until you don't give the answer we need
+//create a while loop with using indexOf tool to make a any answer possible with yes or yeah
 
-while(ans !=="yes" && ans !=="yeah"){
-	var ans = prompt("Are you ready!!?");
-}
-//Throw an alert for the we made it
-	alert("Yes, We made it");
+ while (ans.indexOf("yes")=== -1 && ans.indexOf("yeah")===-1){
+ 	var ans = prompt("are you not there yet!!?")
+ }
 
+ //Throw an alert for the we made it
+
+ alert("Yeah, We made it!!");
