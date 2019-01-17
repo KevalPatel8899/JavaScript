1713e0faac1e590ea018495de5d118ddf6c8dd4e
Keval Patel <200393482@student,georgianc.on.ca>
2019-01-16 19:19:30 -0500

    Functions

--- list.js
+++ list.js
@@ -8,20 +8,53 @@ var inputs= prompt("What do you want to do?? ");
 //to run the command usntil this quit is not used
 while (inputs !== "quit"){
 
-
 	if( inputs === "list")
 	{
-		console.log(todos);
+		listTodos();
 	}else if(inputs === "new")
 	{
-	//To tell the wnter the new todo list	
-	var newTodo = prompt("Enter the new todo");
-	//add the value from the different value to the array
-	todos.push(newTodo);
+		addTodos();
+	}
+	//To delete the index
+	else if(inputs === "delete")
+	{
+		deletTodos();
 	}
 	//ask again for the new input
 var inputs= prompt("What do you want to do?? ");
 }
 
 //End statement
-console.log("Ok, You quit the app!!");
+
+alert("Ok, You quit the app!!");
+
+
+
+// Function for lists
+
+function listTodos(){
+console.log("**********");
+		todos.forEach(function(todo , i )
+		{
+			console.log(i +" : "+todo);
+		});
+		console.log("**********");	
+}
+
+//function for add new todolist
+function addTodos(){
+	//To tell the wnter the new todo list	
+		var newTodo = prompt("Enter the new todo");
+		//add the value from the different value to the array
+		todos.push(newTodo);
+}
+
+//function for delete todos
+function deletTodos(){
+		// enter the todo which the user want too delete
+		var index = prompt("Enter the todo number which you want to delete");
+		//Enter the splice method to delete
+		todos.splice(index,1);
+		//Alert
+		alert("Deleted");
+}
