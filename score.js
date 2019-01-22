6ca127fbfb71e039e5e76affad5bf7c69ad9cf9b
Keval Patel <200393482@student,georgianc.on.ca>
2019-01-22 01:19:25 -0500

    Functions

--- score.js
+++ score.js
@@ -8,32 +8,50 @@ var p2Score = 0;
 var gameOver = false;
 var winnig = 5;
 var winnigDisplay = document.querySelector("p span");
-var numInput = document.querySelector("#number");
+var numInput = document.querySelector("input");
 
+// addEventListener in for the all buttons 
 p1Button.addEventListener("click",function(){
+	player1();
+}); 
+p2Button.addEventListener("click",function(){
+	player2();
+}); 
+
+reset.addEventListener("click",function(){
+ resetButton();
+});
+
+
+// function for the all code to make the dry code
+function player1(){
 	if(!gameOver){
-		p1Score++;
+			p1Score++;
 		if(p1Score === winnig){
 			p1.classList.add("winner");
 			p2.classList.add("looser");
 			gameOver = true;
+			alert("player1 wins the game")
+
 		}
 		p1.textContent =p1Score;
 	}
-}); 
-p2Button.addEventListener("click",function(){
-	if(!gameOver){
-		p2Score++;
+}
+
+function player2(){
+		if(!gameOver){
+			p2Score++;
 		if(p2Score === winnig){
 			p2.classList.add("winner");
 			p1.classList.add("looser");
 			gameOver = true;
+			alert("player2 wins the game")
 		}
 		p2.textContent =p2Score;
-	}
-}); 
+	}	
+}
 
-reset.addEventListener("click",function(){
+function resetButton(){
 p1Score=0;
 p2Score=0
 p1.textContent=0;
@@ -43,5 +61,20 @@ p2.classList.remove("looser");
 p2.classList.remove("winner");
 p1.classList.remove("looser");
 gameOver =false;
-});
+winnigDisplay.textContent = 5;
+winnig = 5;
+numInput.value = 0;
+}
 
+
+//Number input for the change the value as per requirment
+numInput.addEventListener("change",function(){
+winnigDisplay.textContent= numInput.value;
+if(Number(numInput.value) < 0){
+	alert("input number should be greater then 0;");
+	gameOver = true;
+}
+else{
+	winnig= Number(numInput.value);
+}
+});
